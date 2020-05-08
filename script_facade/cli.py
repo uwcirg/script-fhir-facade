6e0354484c6e94cd9523d62e34f40a38bd286cab
Ivan Cvitkovic <ivanc@uw.edu>
2020-05-08 14:17:10 -0700

    Update imports, debugging

--- script_facade/cli.py
+++ script_facade/cli.py
@@ -1,22 +1,27 @@
+import json
+
 from requests import Session
 
-from client.client_106 import RxRequest, session_data
+from client.client_106 import RxRequest, session_data, parse_rx_history_response
 from client.config import DefaultConfig as client_config
 
-from models.r1.medication_order import MedicationOrder, parse_rx_history_response
+
+patient_fname = 'Luke'
+patient_lname = 'Skywalker'
+patient_dob = '1977-01-12'
 
 def main():
     api_endpoint = client_config.SCRIPT_ENDPOINT_URL
 
     request_builder = RxRequest(url=api_endpoint)
-    request = request_builder.build_request()
+    request = request_builder.build_request(patient_fname, patient_lname, patient_dob)
     s = Session()
     response = s.send(request, **session_data)
     xml_body = response.text
 
     meds = parse_rx_history_response(xml_body)
 
-    print(*meds, sep='\n')
+    print(json.dumps(meds, indent=2, separators=(',', ': ')))
 
 if __name__ == "__main__":
     main()
