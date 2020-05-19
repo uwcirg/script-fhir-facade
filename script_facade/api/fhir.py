331b20a0471968264cb8a8ed3008a3eb4d145750
Ivan Cvitkovic <ivanc@uw.edu>
2020-05-19 11:00:10 -0700

    Add todo

--- script_facade/api/fhir.py
+++ script_facade/api/fhir.py
@@ -3,7 +3,10 @@ from script_facade.client.client_106 import rx_history_query
 
 blueprint = Blueprint('fhir', __name__, url_prefix='/v/r2/fhir/')
 
-
+# todo: version-based routing
+# todo: support both types of queries:
+# ?subject:Patient.name.given=FIRST_NAME&subject:Patient.name.family=LAST_NAME&subject:Patient.birthdate=eqYYYY-MM-DD
+# ?patient=PATIENT_ID
 @blueprint.route('/MedicationOrder')
 def medication_order():
     patient_fname = 'Luke'
