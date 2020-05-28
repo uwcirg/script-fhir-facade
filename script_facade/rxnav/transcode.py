091b353cfa89a8462f043329cc50c6e050718cb1
Ivan Cvitkovic <ivanc@uw.edu>
2020-05-28 13:17:05 -0700

    Add docstrings; cleanup

--- script_facade/rxnav/transcode.py
+++ script_facade/rxnav/transcode.py
@@ -1,11 +1,13 @@
 import requests
 
+
 rxnav_system_map = {
     'NDC': 'http://hl7.org/fhir/sid/ndc',
+    'rxnorm': 'http://www.nlm.nih.gov/research/umls/rxnorm',
 }
 
-
 def to_rxnorm(coding, rxnav_url):
+    """Return first RxNorm coding to given NDC coding"""
     reverse_system_map = {v: k for k, v in rxnav_system_map.items()}
     querystring_params = {
         'idtype': reverse_system_map[coding['system']],
@@ -22,14 +24,16 @@ def to_rxnorm(coding, rxnav_url):
 
     coding_fhir = {
         'code': rxnorm_codes[0],
-        'system': 'http://www.nlm.nih.gov/research/umls/rxnorm',
+        'system': rxnav_system_map['rxnorm'],
         'display': coding['display'],
     }
     return coding_fhir
 
 
 def add_rxnorm_coding(medication_order_bundle, rxnav_url):
+    """Add rxnorm codings to each MedicationOrder in given FHIR bundle"""
     for medication_order in medication_order_bundle['entry']:
+        # todo: fixup extra medicationCodeableConcept nesting in FHIR
         medication = medication_order['medicationCodeableConcept']['medicationCodeableConcept']
         rxnorm_codings = []
         for coding in medication['coding']:
@@ -40,11 +44,7 @@ def add_rxnorm_coding(medication_order_bundle, rxnav_url):
             if not rxnorm_coding:
                 continue
 
-            if False:
-                medication['coding'] = []
             rxnorm_codings.append(rxnorm_coding)
-
         medication['coding'].extend(rxnorm_codings)
-
 
     return medication_order_bundle
