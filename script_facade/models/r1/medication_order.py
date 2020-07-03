26257e5b178af330d6327a11cd2a0abf4c6d383f
Ivan Cvitkovic <ivanc@uw.edu>
2020-07-02 19:36:26 -0700

    Remove unused attribute

--- script_facade/models/r1/medication_order.py
+++ script_facade/models/r1/medication_order.py
@@ -1,7 +1,4 @@
 # https://www.hl7.org/fhir/terminologies-systems.html
-# todo: convert from from National Drug Code to RxNorm
-# http://www.nlm.nih.gov/research/umls/rxnorm
-# https://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=NDC_ID
 drug_code_system_map = {
     # todo: interpolate dashes as necessary for NDC
     'ND': 'http://hl7.org/fhir/sid/ndc'
@@ -14,7 +11,6 @@ class MedicationOrder(object):
         self.medication = None
 
         self.date_written = None
-        self.date_ended = None
         self.dispense_request = None
         self.prescriber = None
 
@@ -113,7 +109,6 @@ class MedicationOrder(object):
         fhir_json = {
             'resourceType': 'MedicationOrder',
             'dateWritten': self.date_written,
-            'dateEnded': self.date_ended,
             'medicationCodeableConcept': self.medication,
             'dispenseRequest': self.dispense_request,
             'prescriber': self.prescriber,
