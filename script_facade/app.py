bb559ec3b5d50c5a8cf50989bd936f369cf94b10
Paul Bugni <pbugni@u.washington.edu>
2021-06-15 13:10:12 -0700

    Add requests caching using redis as backing store.

--- script_facade/app.py
+++ script_facade/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import redis
+import requests_cache
 
 from script_facade import api
 
@@ -10,15 +12,37 @@ def create_app(testing=False, cli=False):
     app.config.from_object('script_facade.client.config')
     register_blueprints(app)
     configure_app(app)
+    configure_cache(app)
 
     return app
+
 
 def register_blueprints(app):
     """register all blueprints for application
     """
     app.register_blueprint(api.fhir.blueprint)
 
+
 def configure_app(app):
     """Load successive configs - overriding defaults"""
 
     app.config.from_object('script_facade.config')
+
+
+def configure_cache(app):
+    """Configure caching libraries"""
+
+    # NB this effectively turns caching on for ALL requests API calls.
+    # To temporarily disable, wrap w/ context manager:
+    #
+    #   with requests_cache.disabled():
+    #     requests.get('http://httpbin.org/get')
+
+    requests_cache.install_cache(
+        cache_name=app.name,
+        backend='redis',
+        expire_after=app.config['REQUEST_CACHE_EXPIRE'],
+        include_get_headers=True,
+        old_data_on_error=True,
+        connection=redis.StrictRedis.from_url(app.config.get("REQUEST_CACHE_URL")),
+    )
--- script_facade/config.py
+++ script_facade/config.py
@@ -4,6 +4,15 @@ Use environment variable to override
 """
 import os
 
+REDIS_URL = os.environ.get(
+    'REDIS_URL',
+    'redis://localhost:6379/5' if os.environ.get('TESTING', 'false').lower() == 'true'
+    else 'redis://localhost:6379/0',
+)
+
+REQUEST_CACHE_URL = os.environ.get('REQUEST_CACHE_URL', REDIS_URL)
+REQUEST_CACHE_EXPIRE = 24 * 60 * 60  # 24 hours
+
 SERVER_NAME = os.getenv("SERVER_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
