e9c59be06f1d33995f1e85c7aa4a8c75b96a50d2
Paul Bugni <pbugni@u.washington.edu>
2021-06-16 11:33:11 -0700

    refactor /settings view to `misc.py`

--- script_facade/api/fhir.py
+++ script_facade/api/fhir.py
@@ -1,73 +1,10 @@
-import requests
-import requests_cache
-from time import time
-from flask import Blueprint, jsonify, request, current_app
-from flask.json import JSONEncoder
+from flask import Blueprint, request, current_app
 
 from script_facade.client.client_106 import rx_history_query, patient_lookup_query
 from script_facade.rxnav.transcode import add_rxnorm_coding
 
 
 blueprint = Blueprint('fhir', __name__)
-
-
-@blueprint.route('/test/<x>')
-def test_cache(x):
-    iterations = 60
-    cache_method = None
-    b4 = time()
-    if x == 'x':
-        cache_method = 'CachedSession'
-        session = requests_cache.CachedSession('demo_cache')
-        for i in range(iterations):
-            session.get('http://httpbin.org/delay/1')
-    elif x == 'y':
-        cache_method = 'Uncached Session'
-        session = requests.Session()
-        for i in range(iterations):
-            session.get('http://httpbin.org/delay/1')
-    elif x == 'z':
-        cache_method = 'requests configured cache'
-        for i in range(iterations):
-            requests.get('http://httpbin.org/delay/1')
-    else:
-        cache_method = 'requests context manager (no cache exception)'
-        with requests_cache.disabled():
-            for i in range(iterations):
-                requests.get('http://httpbin.org/delay/1')
-
-    return jsonify(cache_method=cache_method, duration=time()-b4)
-
-
-@blueprint.route('/settings', defaults={'config_key': None})
-@blueprint.route('/settings/<string:config_key>')
-def config_settings(config_key):
-    """Non-secret application settings"""
-
-    # workaround no JSON representation for datetime.timedelta
-    class CustomJSONEncoder(JSONEncoder):
-        def default(self, obj):
-            return str(obj)
-    current_app.json_encoder = CustomJSONEncoder
-
-    # return selective keys - not all can be be viewed by users, e.g.secret key
-    blacklist = ('SECRET', 'KEY')
-
-    if config_key:
-        key = config_key.upper()
-        for pattern in blacklist:
-            if pattern in key:
-                abort(400, f"Configuration key {key} not available")
-        return jsonify({key: current_app.config.get(key)})
-
-    config_settings = {}
-    for key in current_app.config:
-        matches = any(pattern for pattern in blacklist if pattern in key)
-        if matches:
-            continue
-        config_settings[key] = current_app.config.get(key)
-
-    return jsonify(config_settings)
 
 
 # todo: version-based routing
@@ -84,7 +21,6 @@ def medication_order(fhir_version):
     patient_lname = request.args.get('subject:Patient.name.family', default_patient_lname)
     patient_dob = request.args.get('subject:Patient.birthdate', default_patient_dob).split('eq')[-1]
 
-
     med_order_bundle = rx_history_query(
         patient_fname=patient_fname,
         patient_lname=patient_lname,
@@ -99,6 +35,7 @@ def medication_order(fhir_version):
         )
 
     return med_order_bundle
+
 
 # todo: version-based routing
 # todo: support both types of queries:
---
+++ script_facade/api/misc.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, abort, current_app, jsonify
+from flask.json import JSONEncoder
+
+blueprint = Blueprint('misc', __name__)
+
+
+@blueprint.route('/settings', defaults={'config_key': None})
+@blueprint.route('/settings/<string:config_key>')
+def config_settings(config_key):
+    """Non-secret application settings"""
+
+    # workaround no JSON representation for datetime.timedelta
+    class CustomJSONEncoder(JSONEncoder):
+        def default(self, obj):
+            return str(obj)
+    current_app.json_encoder = CustomJSONEncoder
+
+    # return selective keys - not all can be be viewed by users, e.g.secret key
+    blacklist = ('SECRET', 'KEY')
+
+    if config_key:
+        key = config_key.upper()
+        for pattern in blacklist:
+            if pattern in key:
+                abort(400, f"Configuration key {key} not available")
+        return jsonify({key: current_app.config.get(key)})
+
+    results = {}
+    for key in current_app.config:
+        matches = any(pattern for pattern in blacklist if pattern in key)
+        if matches:
+            continue
+        results[key] = current_app.config.get(key)
+
+    return jsonify(results)
